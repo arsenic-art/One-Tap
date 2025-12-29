55ae4bf4ecc0968c8b391fee60eda1912750201b
arsenic-art <piyush782005@gmail.com>
2025-12-29 22:53:59 +0530

    renaming

--- Frontend/src/utils/getAvatar.jsx
+++ Frontend/src/utils/getAvatar.jsx
@@ -1,13 +1,13 @@
-import avatar1 from "../assets/avatars/avatar1.jpg";
-import avatar2 from "../assets/avatars/avatar2.jpg";
-import avatar3 from "../assets/avatars/avatar3.jpg";
-import avatar4 from "../assets/avatars/avatar4.jpg";
-import avatar5 from "../assets/avatars/avatar5.jpg";
-import avatar6 from "../assets/avatars/avatar6.jpg";
-import avatar7 from "../assets/avatars/avatar7.jpg";
-import avatar8 from "../assets/avatars/avatar8.jpg";
-import avatar9 from "../assets/avatars/avatar9.jpg";
-import avatar10 from "../assets/avatars/avatar10.jpg";
+import avatar1 from "../assets/avatars/Avatar1.jpg";
+import avatar2 from "../assets/avatars/Avatar2.jpg";
+import avatar3 from "../assets/avatars/Avatar3.jpg";
+import avatar4 from "../assets/avatars/Avatar4.jpg";
+import avatar5 from "../assets/avatars/Avatar5.jpg";
+import avatar6 from "../assets/avatars/Avatar6.jpg";
+import avatar7 from "../assets/avatars/Avatar7.jpg";
+import avatar8 from "../assets/avatars/Avatar8.jpg";
+import avatar9 from "../assets/avatars/Avatar9.jpg";
+import avatar10 from "../assets/avatars/Avatar10.jpg";
 
 const avatars = [
   avatar1,

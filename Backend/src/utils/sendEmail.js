01be5076957e8bb7c74bf8fac017e1b586b62122
arsenic-art <piyush782005@gmail.com>
2025-12-30 00:09:03 +0530

    updated

--- Backend/src/utils/sendEmail.js
+++ Backend/src/utils/sendEmail.js
@@ -4,7 +4,7 @@ const resend = new Resend(process.env.RESEND_API_KEY);
 
 const sendEmail = async ({ to, subject, html }) => {
   await resend.emails.send({
-    from: "OneTap <no-reply@onetap.dev>",
+    from: "OneTap <onboarding@resend.dev>",
     to,
     subject,
     html,

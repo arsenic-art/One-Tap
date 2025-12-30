6df824c3da2c55baa13f390e19e5f0fef8602049
arsenic-art <piyush782005@gmail.com>
2025-12-30 08:03:32 +0530

    fixing things

--- Backend/src/utils/sendEmail.js
+++ Backend/src/utils/sendEmail.js
@@ -1,10 +1,18 @@
-const { Resend } = require("resend");
+const nodemailer = require("nodemailer");
 
-const resend = new Resend(process.env.RESEND_API_KEY);
+const transporter = nodemailer.createTransport({
+  host: "smtp.gmail.com",
+  port: 465,        
+  secure: true,     
+  auth: {
+    user: process.env.EMAIL_USER,
+    pass: process.env.EMAIL_PASS,
+  },
+});
 
 const sendEmail = async ({ to, subject, html }) => {
-  await resend.emails.send({
-    from: "OneTap <onboarding@resend.dev>",
+  await transporter.sendMail({
+    from: `"OneTap" <${process.env.EMAIL_USER}>`,
     to,
     subject,
     html,
